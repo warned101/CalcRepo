e77ec6b0dc93c8eb0b927a8994f77781b551c736
Astitva Gupta <astitvagupta123@gmail.com>
2020-08-07 16:35:41 +0530

    Calc created

---
+++ Calc.java
@@ -0,0 +1,21 @@
+class Calc{  
+public static int add(int a,int b){  
+return a+b;  
+}  
+public static int sub(int a,int b){  
+return a-b;  
+}  
+public static int mul(int a,int b){  
+return a*b;  
+}  
+public static int div(int a,int b){  
+return a/b;  
+}  
+public static void main(String []args){  
+System.out.println(add(10,20));  
+System.out.println(sub(10,20));  
+System.out.println(mul(10,20));  
+System.out.println(div(10,20));  
+}  
+}  
+© 2020 GitHub, Inc.
