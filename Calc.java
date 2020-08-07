3876cee2e6430cf7ed6aba4310ba06817d8fe68d
Astitva Gupta <astitvagupta123@gmail.com>
2020-08-07 18:13:16 +0530

    Commented hello

--- Calc.java
+++ Calc.java
@@ -16,7 +16,7 @@ System.out.println(add(10,20));
 System.out.println(sub(10,20));  
 System.out.println(mul(10,20));  
 System.out.println(div(10,20));  
-  System.out.println("Hello");
+//   System.out.println("Hello");
 }  
 }  
 
