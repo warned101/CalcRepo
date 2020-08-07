e6dcb071c2605cb3e1b89b42204ee3f2015a3fe8
Astitva Gupta <astitvagupta123@gmail.com>
2020-08-07 17:54:26 +0530

    Removed div from CalcRepo

--- Calc.java
+++ Calc.java
@@ -8,14 +8,14 @@ return a-b;
 public static int mul(int a,int b){  
 return a*b;  
 }  
-public static int div(int a,int b){  
-return a/b;  
-}  
+// public static int div(int a,int b){  
+// return a/b;  
+// }  
 public static void main(String []args){  
 System.out.println(add(10,20));  
 System.out.println(sub(10,20));  
 System.out.println(mul(10,20));  
-System.out.println(div(10,20));  
+// System.out.println(div(10,20));  
 }  
 }  
 
